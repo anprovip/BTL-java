54c7436a0e47e624f830c0877daa9fb035fc689c
anprovip <antit1616@gmail.com>
2024-04-17 17:20:28 +0700

    fix bug
    fix thành công bug khi chia file

--- src/module-info.java
+++ src/module-info.java
@@ -7,4 +7,7 @@ module BTL {
 	requires java.desktop;
 	
 	opens application to javafx.graphics, javafx.fxml;
+	opens controller to javafx.graphics, javafx.fxml,java.sql;
+	opens database to javafx.graphics, javafx.fxml,java.sql;
+	opens model to javafx.graphics, javafx.fxml,java.sql,javafx.controls,javafx.base;
 }
