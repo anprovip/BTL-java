2a067b27309292de7802e0e89906e50e733839ef
Vo Nghia An <annghiavo@gmail.com>
2024-05-05 09:42:34 +0700

    add thêm thể loại sách

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -12,12 +12,15 @@ import java.util.ResourceBundle;
 import javax.swing.JOptionPane;
 
 import database.DAOUser;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -67,6 +70,9 @@ public class UserController implements Initializable{
 
     @FXML
     private TextField isbn;
+    
+    @FXML
+    private Label selection;
 
     @FXML
     private ListView<String> listView;
@@ -317,6 +323,18 @@ public class UserController implements Initializable{
 		myShelvesPageController = MyShelvesPageController.getInstance();
 		displayName.setText(displayName.getText());
 		instance = this;
+		
+		String[] items = {"Romance","fantasy", "Horror", "Fiction", "Classics"};
+		listView.getItems().addAll(items);
+		
+		listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+		listView.getSelectionModel().selectedItemProperty().addListener(this::selectionChanged);
+	}
+	
+	private void selectionChanged(ObservableValue<? extends String> Obsevable,String oldVal, String newVal) {
+		ObservableList<String> selectedItems = listView.getSelectionModel().getSelectedItems();
+		String getSelectedItem = (selectedItems.isEmpty())?"Nothing selected":selectedItems.toString();
+		selection.setText(getSelectedItem);
 		
 	}
 
